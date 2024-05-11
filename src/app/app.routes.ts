d26bc3386612e2a07c8475628d5baeae5b1c53c0
Maicol D. Mamani Chalco <mdmch13@gmail.com>
2024-05-10 22:22:30 -0300

    Accediendo a los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+
+
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', redirectTo:'agregar-empleado'},
+    {path: 'agregar-empleado', component:AgregarEmpleadoComponent},
+    {path: 'listar-empleado', component:ListarEmpleadoComponent},
+    {path: 'editar-empleado/:id', component:EditarEmpleadoComponent}
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
